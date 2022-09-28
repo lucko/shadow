80576c5c63a87e2d51e1277d0cc2ca668a218eb4
Dysaido <tonyyoni@gmail.com>
2022-09-28 23:22:42 +0200

    Update PrivateMethodHandles for newer jre (Remove reflection)

--- src/main/java/me/lucko/shadow/PrivateMethodHandles.java
+++ src/main/java/me/lucko/shadow/PrivateMethodHandles.java
@@ -36,16 +36,6 @@ import java.lang.reflect.InvocationTargetException;
  */
 final class PrivateMethodHandles {
 
-    private static final @NonNull Constructor<MethodHandles.Lookup> LOOKUP_CONSTRUCTOR;
-    static {
-        try {
-            LOOKUP_CONSTRUCTOR = MethodHandles.Lookup.class.getDeclaredConstructor(Class.class, int.class);
-            LOOKUP_CONSTRUCTOR.setAccessible(true);
-        } catch (NoSuchMethodException e) {
-            throw new ExceptionInInitializerError(e);
-        }
-    }
-
     /**
      * Returns a {@link MethodHandles.Lookup lookup object} with full capabilities to emulate all
      * supported bytecode behaviors, including private access, on a target class.
@@ -54,11 +44,7 @@ final class PrivateMethodHandles {
      * @return a lookup object for the target class, with private access
      */
     public static MethodHandles.@NonNull Lookup forClass(@NonNull Class<?> targetClass) {
-        try {
-            return LOOKUP_CONSTRUCTOR.newInstance(targetClass, MethodHandles.Lookup.PUBLIC | MethodHandles.Lookup.PRIVATE | MethodHandles.Lookup.PROTECTED | MethodHandles.Lookup.PACKAGE);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
+        MethodHandles.lookup().in(targetClass);
     }
 
 }
